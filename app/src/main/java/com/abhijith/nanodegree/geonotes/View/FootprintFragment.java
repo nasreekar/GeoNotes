861a2f70f4bd0c0dfdcbb34d6cbc46d2daf965f8
Abhijith Sreekar <abhijith.sreekar@infra.tigerspike.com>
2019-08-13 16:52:30 +0800

    Implement google maps with hard coded location and show a marker

--- app/src/main/java/com/abhijith/nanodegree/geonotes/View/FootprintFragment.java
+++ app/src/main/java/com/abhijith/nanodegree/geonotes/View/FootprintFragment.java
@@ -8,14 +8,54 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.abhijith.nanodegree.geonotes.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class FootprintFragment extends Fragment {
+public class FootprintFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    public FootprintFragment() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_footprint, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_footprint, container, false);
+
+        //use SupportMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg_footprint);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+
+        CameraPosition googlePlex = CameraPosition.builder()
+                .target(sydney)
+                .zoom(10)
+                .bearing(0)
+                .tilt(45)
+                .build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 2000, null);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
     }
 }
