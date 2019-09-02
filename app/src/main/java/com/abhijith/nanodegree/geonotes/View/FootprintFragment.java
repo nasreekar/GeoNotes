36e05b07a85bb07ba2bdbbb2041d5a93ace0b781
Abhijith Sreekar <nasreekar@gmail.com>
2019-09-02 17:30:12 +1000

    Implement places autocompletion api

--- app/src/main/java/com/abhijith/nanodegree/geonotes/View/FootprintFragment.java
+++ app/src/main/java/com/abhijith/nanodegree/geonotes/View/FootprintFragment.java
@@ -7,14 +7,11 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,18 +20,25 @@ import androidx.fragment.app.Fragment;
 
 import com.abhijith.nanodegree.geonotes.Model.GooglePlayServicesHelper;
 import com.abhijith.nanodegree.geonotes.R;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -46,8 +50,8 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
 
     private static final float DEFAULT_ZOOM = 15f;
 
-    @BindView(R.id.input_search)
-    AutoCompleteTextView mSearchText;
+    @BindView(R.id.tv_search)
+    TextView mSearchText;
 
     @BindView(R.id.ic_gps)
     ImageView mGps;
@@ -55,11 +59,13 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
     private Location currentLocation;
 
-    private boolean mLocationPersmissionsGranted = false;
+    private boolean mLocationPermissionsGranted = false;
 
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATIONS_PERMISSIONS_REQUEST = 5445;
+
+    private AutocompleteSupportFragment autocompleteFragment;
 
 
     public FootprintFragment() {
@@ -68,8 +74,10 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onResume() {
         super.onResume();
-        if (GooglePlayServicesHelper.isGooglePlayServicesAvailable(this.getContext())) {
-            getLocationPermissions();
+        if (!mLocationPermissionsGranted) {
+            if (GooglePlayServicesHelper.isGooglePlayServicesAvailable(this.getContext())) {
+                getLocationPermissions();
+            }
         }
     }
 
@@ -88,17 +96,34 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
     private void initMap() {
         Log.d(TAG, "initMap: initializing map fragment");
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg_footprint);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getActivity().getApplicationContext(), getString(R.string.API_KEY));
+        }
+
+        // Initialize the AutocompleteSupportFragment.
+        autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        if (autocompleteFragment != null) {
+            autocompleteFragment.setHint("Search Location");
+            autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+        }
+
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(getActivity(), "onMapReady: Map is ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: Map is ready");
         this.mMap = googleMap;
 
-        if (mLocationPersmissionsGranted) {
+        if (mLocationPermissionsGranted) {
             Log.d(TAG, "onMapReady: Getting current location of the device and moving the camera");
+
             getDeviceLocation();
+
             if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED) {
@@ -113,7 +138,23 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
 
     private void init() {
         Log.d(TAG, "init: initializing");
-        mSearchText.setOnEditorActionListener((textView, actionId, keyEvent) -> {
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                mSearchText.setText(place.getName());
+                geoLocate();
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+            }
+
+        });
+
+       /* mSearchText.setOnEditorActionListener((textView, actionId, keyEvent) -> {
             if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE
                     || keyEvent.getAction() == KeyEvent.ACTION_DOWN
                     || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER) {
@@ -122,25 +163,25 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
                 geoLocate();
             }
             return false;
-        });
+        });*/
 
         mGps.setOnClickListener(view -> {
             Log.d(TAG, "onClick: clicked gps icon");
             getDeviceLocation();
         });
-
-        hideSoftKeyboard();
     }
 
     private void geoLocate() {
         Log.d(TAG, "geoLocate: geolocating");
         String searchString = mSearchText.getText().toString();
+        Log.d(TAG, "geoLocate: searchString: " + searchString);
         Geocoder geocoder = new Geocoder(this.getActivity());
         List<Address> list = new ArrayList<>();
         try {
             list = geocoder.getFromLocationName(searchString, 1);
         } catch (IOException e) {
             Log.d(TAG, "geoLocate: IOException: " + e.getMessage());
+            Toast.makeText(getActivity(), getString(R.string.error_retrieving_location_coordinates), Toast.LENGTH_SHORT).show();
         }
 
         if (list.size() > 0) {
@@ -154,7 +195,7 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
         Log.d(TAG, "getDeviceLocation: getting device's current location");
         FusedLocationProviderClient mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
         try {
-            if (mLocationPersmissionsGranted) {
+            if (mLocationPermissionsGranted) {
                 Task location = mfusedLocationProviderClient.getLastLocation();
                 location.addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
@@ -165,7 +206,7 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
 
                     } else {
                         Log.d(TAG, "onComplete: current location is null");
-                        Toast.makeText(getActivity(), "Unable to get current location", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), getString(R.string.current_location_not_found), Toast.LENGTH_SHORT).show();
                     }
                 });
             }
@@ -181,7 +222,7 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
                 FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(),
                     COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                mLocationPersmissionsGranted = true;
+                mLocationPermissionsGranted = true;
                 initMap();
             } else {
                 ActivityCompat.requestPermissions(getActivity(), permissions, LOCATIONS_PERMISSIONS_REQUEST);
@@ -194,7 +235,7 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         Log.d(TAG, "onRequestPermissionResult: called");
-        mLocationPersmissionsGranted = false;
+        mLocationPermissionsGranted = false;
         if (requestCode == LOCATIONS_PERMISSIONS_REQUEST) {
             if (grantResults.length > 0) {
                 for (int grantResult : grantResults) {
@@ -204,7 +245,7 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
                     }
                 }
                 Log.d(TAG, "onRequestPermissionsResult: permission granted");
-                mLocationPersmissionsGranted = true;
+                mLocationPermissionsGranted = true;
                 // initialize our map
                 initMap();
             }
@@ -213,7 +254,6 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
 
     private void moveCamera(LatLng latLng, String title) {
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
 
         if (!title.equals("My Location")) {
             MarkerOptions options = new MarkerOptions()
@@ -222,10 +262,13 @@ public class FootprintFragment extends Fragment implements OnMapReadyCallback {
             mMap.addMarker(options);
         }
 
-        hideSoftKeyboard();
+        //Zoom in and animate the camera.
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(getCameraPositionWithBearing(latLng)));
+
     }
 
-    private void hideSoftKeyboard() {
-        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    @NonNull
+    private CameraPosition getCameraPositionWithBearing(LatLng latLng) {
+        return new CameraPosition.Builder().target(latLng).zoom(DEFAULT_ZOOM).build();
     }
 }
